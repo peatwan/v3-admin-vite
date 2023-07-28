a8d1b613505421da34af66163de00c6e67bde864
xiongdi <xiongdi94@outlook.com>
2023-07-28 18:08:00 +0800

    wip: 完善 devextreme 表格

--- src/views/table/devextreme/index.vue
+++ src/views/table/devextreme/index.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="app-container">
     <div class="search-wrapper">
-      <DxTextBox :value="username" :max-length="40" styling-mode="outlined" label="用户名" />
-      <DxTextBox :value="phone" :max-length="40" styling-mode="outlined" label="手机号" />
+      <DxTextBox v-model="searchData.username" :max-length="40" styling-mode="outlined" label="用户名" />
+      <DxTextBox v-model="searchData.phone" :max-length="40" styling-mode="outlined" label="手机号" />
       <DxButton class="action-button" text="查询" styling-mode="contained" type="default" icon="search" />
-      <DxButton class="action-button" text="重置" styling-mode="contained" type="normal" icon="revert" />
+      <DxButton
+        class="action-button"
+        text="重置"
+        styling-mode="contained"
+        type="normal"
+        icon="revert"
+        @click="resetSearch"
+      />
     </div>
-    <div class="table-wrapper">
+    <div class="data-grid-wrapper">
       <div class="toolbar-wrapper">
         <div class="buttons">
           <DxButton text="新增用户" styling-mode="contained" type="default" icon="add" />
@@ -17,61 +24,220 @@
           <DxButton styling-mode="contained" icon="refresh" />
         </div>
       </div>
-      <div>
-        <DxDataGrid :allow-column-reordering="true" :data-source="customers" key-expr="ID" :show-borders="false">
-          <DxColumn data-field="CompanyName" />
-          <DxColumn data-field="Phone" />
-          <DxColumn data-field="Fax" />
-          <DxColumn data-field="City" />
-          <DxColumn :group-index="0" data-field="State" />
+      <div class="table-wrapper">
+        <DxDataGrid
+          :allow-column-reordering="true"
+          :data-source="tableData"
+          key-expr="id"
+          :show-borders="false"
+          :columnAutoWidth="true"
+          :showRowLines="true"
+          :showColumnLines="false"
+        >
+          <DxHeaderFilter :visible="true" />
+          <DxColumn data-field="username" caption="用户名" />
+          <DxColumn data-field="roles" caption="角色" cell-template="role-cell" />
+          <template #role-cell="{ data }">
+            <el-tag v-if="data.data.roles === 'admin'" effect="plain">admin</el-tag>
+            <el-tag v-else type="warning" effect="plain">{{ data.data.roles }}</el-tag>
+          </template>
+
+          <DxColumn data-field="phone" caption="手机号" />
+          <DxColumn data-field="email" caption="邮箱" />
+          <DxColumn data-field="status" caption="状态" cell-template="status-cell" />
+          <template #status-cell="{ data }">
+            <el-tag v-if="data.data.status" type="success" effect="plain">启用</el-tag>
+            <el-tag v-else type="danger" effect="plain">禁用</el-tag>
+          </template>
+          <DxColumn data-field="createTime" caption="创建时间" />
+          <DxColumn caption="操作" cell-template="action-cell" />
+          <template #action-cell="{ data }">
+            <el-button type="primary" text bg size="small" @click="handleUpdate(data.data)">修改</el-button>
+            <el-button type="danger" text bg size="small" @click="handleDelete(data.data)">删除</el-button>
+          </template>
 
           <DxGroupPanel :visible="true" />
-          <DxGrouping :auto-expand-all="autoExpandAll" />
-          <DxPaging :page-size="10" />
+          <DxGrouping />
           <DxSearchPanel :visible="true" />
+          <DxSelection mode="multiple" />
         </DxDataGrid>
       </div>
-
-      <div class="options">
-        <div class="caption">Options</div>
-        <div class="option">
-          <DxCheckBox id="autoExpand" v-model:value="autoExpandAll" text="Expand All Groups" />
-        </div>
+      <div class="pager-wrapper">
+        <el-pagination
+          background
+          :layout="paginationData.layout"
+          :page-sizes="paginationData.pageSizes"
+          :total="paginationData.total"
+          :page-size="paginationData.pageSize"
+          :currentPage="paginationData.currentPage"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        />
       </div>
     </div>
+    <!-- 新增/修改 -->
+    <el-dialog
+      v-model="dialogVisible"
+      :title="currentUpdateId === undefined ? '新增用户' : '修改用户'"
+      @close="resetForm"
+      width="30%"
+    >
+      <el-form ref="formRef" :model="formData" :rules="formRules" label-width="100px" label-position="left">
+        <el-form-item prop="username" label="用户名">
+          <el-input v-model="formData.username" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item prop="password" label="密码" v-if="currentUpdateId === undefined">
+          <el-input v-model="formData.password" placeholder="请输入" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="handleCreate">确认</el-button>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
-<script>
-import DxCheckBox from "devextreme-vue/check-box"
-import { DxDataGrid, DxColumn, DxGrouping, DxGroupPanel, DxSearchPanel, DxPaging } from "devextreme-vue/data-grid"
-import { customers } from "./data.js"
+<script lang="ts" setup>
+import { reactive, ref, watch } from "vue"
+import { createTableDataApi, deleteTableDataApi, updateTableDataApi, getTableDataApi } from "@/api/table"
+import { type GetTableData } from "@/api/table/types/table"
+import { usePagination } from "@/hooks/usePagination"
+import { type FormInstance, type FormRules, ElMessage, ElMessageBox } from "element-plus"
+
+import {
+  DxDataGrid,
+  DxColumn,
+  DxGrouping,
+  DxGroupPanel,
+  DxSearchPanel,
+  DxSelection,
+  DxHeaderFilter
+} from "devextreme-vue/data-grid"
 import DxTextBox from "devextreme-vue/text-box"
 import DxButton from "devextreme-vue/button"
 
-export default {
-  components: {
-    DxCheckBox,
-    DxColumn,
-    DxGroupPanel,
-    DxGrouping,
-    DxPaging,
-    DxSearchPanel,
-    DxDataGrid,
-    DxTextBox,
-    DxButton
-  },
-  name: "DevExtremeTable",
-  data() {
-    return {
-      autoExpandAll: true,
-      customers,
-      username: "",
-      phone: ""
+defineOptions({
+  // 命名当前组件
+  name: "DevExtremeTable"
+})
+
+const loading = ref<boolean>(false)
+const { paginationData, handleCurrentChange, handleSizeChange } = usePagination()
+
+//#region 增
+const dialogVisible = ref<boolean>(false)
+const formRef = ref<FormInstance | null>(null)
+const formData = reactive({
+  username: "",
+  password: ""
+})
+const formRules: FormRules = reactive({
+  username: [{ required: true, trigger: "blur", message: "请输入用户名" }],
+  password: [{ required: true, trigger: "blur", message: "请输入密码" }]
+})
+const handleCreate = () => {
+  formRef.value?.validate((valid: boolean, fields) => {
+    if (valid) {
+      if (currentUpdateId.value === undefined) {
+        createTableDataApi(formData)
+          .then(() => {
+            ElMessage.success("新增成功")
+            getTableData()
+          })
+          .finally(() => {
+            dialogVisible.value = false
+          })
+      } else {
+        updateTableDataApi({
+          id: currentUpdateId.value,
+          username: formData.username
+        })
+          .then(() => {
+            ElMessage.success("修改成功")
+            getTableData()
+          })
+          .finally(() => {
+            dialogVisible.value = false
+          })
+      }
+    } else {
+      console.error("表单校验不通过", fields)
     }
-  }
+  })
 }
+const resetForm = () => {
+  currentUpdateId.value = undefined
+  formData.username = ""
+  formData.password = ""
+}
+//#endregion
+
+//#region 删
+const handleDelete = (row: GetTableData) => {
+  ElMessageBox.confirm(`正在删除用户：${row.username}，确认删除？`, "提示", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    type: "warning"
+  }).then(() => {
+    deleteTableDataApi(row.id).then(() => {
+      ElMessage.success("删除成功")
+      getTableData()
+    })
+  })
+}
+//#endregion
+
+//#region 改
+const currentUpdateId = ref<undefined | string>(undefined)
+const handleUpdate = (row: GetTableData) => {
+  currentUpdateId.value = row.id
+  formData.username = row.username
+  dialogVisible.value = true
+}
+//#endregion
+
+//#region 查
+const tableData = ref<GetTableData[]>([])
+// const searchFormRef = ref<FormInstance | null>(null)
+const searchData = reactive({
+  username: "",
+  phone: ""
+})
+const getTableData = () => {
+  loading.value = true
+  getTableDataApi({
+    currentPage: paginationData.currentPage,
+    size: paginationData.pageSize,
+    username: searchData.username || undefined,
+    phone: searchData.phone || undefined
+  })
+    .then((res) => {
+      paginationData.total = res.data.total
+      tableData.value = res.data.list
+    })
+    .catch(() => {
+      tableData.value = []
+    })
+    .finally(() => {
+      loading.value = false
+    })
+}
+const handleSearch = () => {
+  paginationData.currentPage === 1 ? getTableData() : (paginationData.currentPage = 1)
+}
+const resetSearch = () => {
+  searchData.username = ""
+  searchData.phone = ""
+  handleSearch()
+}
+
+//#endregion
+
+/** 监听分页参数的变化 */
+watch([() => paginationData.currentPage, () => paginationData.pageSize], getTableData, { immediate: true })
 </script>
+
 <style lang="scss" scoped>
 .options {
   padding: 20px;
@@ -100,7 +266,7 @@ export default {
   }
 }
 
-.table-wrapper {
+.data-grid-wrapper {
   margin-bottom: 20px;
   padding: 15px;
   background-color: rgb(255, 255, 255);
@@ -114,10 +280,12 @@ export default {
       display: flex;
     }
   }
-}
-
-.pager-wrapper {
-  display: flex;
-  justify-content: flex-end;
+  .table-wrapper {
+    margin-bottom: 20px;
+  }
+  .pager-wrapper {
+    display: flex;
+    justify-content: flex-end;
+  }
 }
 </style>
